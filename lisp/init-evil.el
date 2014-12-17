52b006c4d27f263aaee757fe349e24d58d649d99
BuerYn <mofy2yx@gmail.com>
2014-12-18 07:37:26 +0800

    together all .el files into lisp

---
+++ lisp/init-evil.el
@@ -0,0 +1,129 @@
+;; load undo-tree and ert
+(add-to-list 'load-path "~/.emacs.d/site-lisp/evil/lib")
+(require 'evil)
+(evil-mode 1)
+
+;; {{@see https://github.com/timcharper/evil-surround
+(require 'surround)
+(global-surround-mode 1)
+;; }}
+
+(defun toggle-org-or-message-mode ()
+  (interactive)
+  (if (eq major-mode 'message-mode)
+    (org-mode)
+    (if (eq major-mode 'org-mode) (message-mode))
+    ))
+
+;; (evil-set-initial-state 'org-mode 'emacs)
+;; Remap org-mode meta keys for convenience
+(evil-declare-key 'normal org-mode-map
+    "gh" 'outline-up-heading
+    "gl" 'outline-next-visible-heading
+    "H" 'org-beginning-of-line ; smarter behaviour on headlines etc.
+    "L" 'org-end-of-line ; smarter behaviour on headlines etc.
+    "$" 'org-end-of-line ; smarter behaviour on headlines etc.
+    "^" 'org-beginning-of-line ; ditto
+    "-" 'org-ctrl-c-minus ; change bullet style
+    "<" 'org-metaleft ; out-dent
+    ">" 'org-metaright ; indent
+    (kbd "TAB") 'org-cycle
+    )
+
+(loop for (mode . state) in
+      '(
+        (Info-mode . emacs)
+        (term-mode . emacs)
+        (log-edit-mode . emacs)
+        (inf-ruby-mode . emacs)
+        (yari-mode . emacs)
+        (erc-mode . emacs)
+        (gud-mode . emacs)
+        (help-mode . emacs)
+        (eshell-mode . emacs)
+        (shell-mode . emacs)
+        ;;(message-mode . emacs)
+        (magit-log-edit-mode . emacs)
+        (fundamental-mode . emacs)
+        (gtags-select-mode . emacs)
+        (weibo-timeline-mode . emacs)
+        (weibo-post-mode . emacs)
+        (sr-mode . emacs)
+        (dired-mode . emacs)
+        (compilation-mode . emacs)
+        (speedbar-mode . emacs)
+        (magit-commit-mode . normal)
+        )
+      do (evil-set-initial-state mode state))
+
+(define-key evil-ex-completion-map (kbd "M-p") 'previous-complete-history-element)
+(define-key evil-ex-completion-map (kbd "M-n") 'next-complete-history-element)
+
+(define-key evil-normal-state-map "Y" (kbd "y$"))
+(define-key evil-normal-state-map "+" 'evil-numbers/inc-at-pt)
+(define-key evil-normal-state-map "-" 'evil-numbers/dec-at-pt)
+(define-key evil-normal-state-map "go" 'goto-char)
+
+(global-evil-matchit-mode 1)
+
+(eval-after-load "evil" '(setq expand-region-contract-fast-key "z"))
+
+;; @see http://stackoverflow.com/questions/10569165/how-to-map-jj-to-esc-in-emacs-evil-mode
+;; @see http://zuttobenkyou.wordpress.com/2011/02/15/some-thoughts-on-emacs-and-vim/
+;;(define-key evil-insert-state-map "k" #'cofi/maybe-exit)
+;;(evil-define-command cofi/maybe-exit ()
+;;  :repeat change
+;;  (interactive)
+;;  (let ((modified (buffer-modified-p)))
+;;    (insert "k")
+;;    (let ((evt (read-event (format "Insert %c to exit insert state" ?j)
+;;               nil 0.5)))
+;;      (cond
+;;       ((null evt) (message ""))
+;;       ((and (integerp evt) (char-equal evt ?j))
+;;    (delete-char -1)
+;;    (set-buffer-modified-p modified)
+;;    (push 'escape unread-command-events))
+;;       (t (setq unread-command-events (append unread-command-events
+;;                          (list evt))))))))
+
+(define-key evil-insert-state-map (kbd "C-p") 'previous-line)
+(define-key evil-insert-state-map (kbd "C-n") 'next-line)
+(define-key evil-insert-state-map (kbd "C-f") 'forward-char)
+(define-key evil-insert-state-map (kbd "C-b") 'backward-char)
+(define-key evil-insert-state-map (kbd "M-f") 'forward-word)
+(define-key evil-insert-state-map (kbd "M-b") 'backward-word)
+
+(define-key evil-insert-state-map (kbd "M-a") 'move-beginning-of-line)
+(define-key evil-insert-state-map (kbd "C-e") 'move-end-of-line)
+(define-key evil-insert-state-map (kbd "M-e") 'move-end-of-line)
+(define-key evil-insert-state-map (kbd "C-k") 'kill-line)
+(define-key evil-insert-state-map (kbd "M-k") 'evil-normal-state)
+(define-key evil-visual-state-map (kbd "M-k") 'evil-exit-visual-state)
+(define-key evil-visual-state-map (kbd ",k") 'evil-exit-visual-state)
+(define-key minibuffer-local-map (kbd "M-k") 'abort-recursive-edit)
+(define-key minibuffer-local-map (kbd ",k") 'abort-recursive-edit)
+(define-key evil-insert-state-map (kbd "M-j") 'my-yas-expand)
+(global-set-key (kbd "M-k") 'keyboard-quit)
+
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+
+
+(defun evilcvn-change-symbol-in-defun ()
+  "mark the region in defun (definition of function) and use string replacing UI in evil-mode
+to replace the symbol under cursor"
+  (interactive)
+  (let ((old (thing-at-point 'symbol)))
+    (mark-defun)
+    (unless (evil-visual-state-p)
+      (evil-visual-state))
+    (evil-ex (concat "'<,'>s/" (if (= 0 (length old)) "" "\\<\\(") old (if (= 0 (length old)) "" "\\)\\>/"))))
+  )
+(global-set-key (kbd "C-c ; s") 'evilcvn-change-symbol-in-defun)
+(provide 'init-evil)
---
+++ lisp/init-spelling.el
@@ -0,0 +1,77 @@
+;; flyspell set up for web-mode
+(defun web-mode-flyspefll-verify ()
+  (let ((f (get-text-property (- (point) 1) 'face)))
+    (not (memq f '(web-mode-html-attr-value-face
+                   web-mode-html-tag-face
+                   web-mode-html-attr-name-face
+                   web-mode-doctype-face
+                   web-mode-keyword-face
+                   web-mode-function-name-face
+                   web-mode-variable-name-face
+                   web-mode-css-property-name-face
+                   web-mode-css-selector-face
+                   web-mode-css-color-face
+                   web-mode-type-face
+                   web-mode-block-control-face
+                   )
+               ))
+    ))
+(put 'web-mode 'flyspell-mode-predicate 'web-mode-flyspefll-verify)
+
+(require 'flyspell-lazy)
+(flyspell-lazy-mode 1)
+;; better performance
+(setq flyspell-issue-message-flag nil)
+
+;; if (aspell installed) { use aspell}
+;; else if (hunspell installed) { use hunspell }
+;; whatever spell checker I use, I always use English dictionary
+;; I prefer use aspell because:
+;; 1. aspell is older
+;; 2. looks Kevin Atkinson still get some roadmap for aspell:
+;; @see http://lists.gnu.org/archive/html/aspell-announce/2011-09/msg00000.html
+(if (executable-find "aspell")
+  (progn
+    (setq ispell-program-name "aspell"
+          ;; ispell-extra-args '("--sug-mode=ultra")
+          ;; force the English dictionary
+          ispell-extra-args '("--sug-mode=ultra" "--lang=en_US")
+          )
+    )
+  (if (executable-find "hunspell")
+    (progn
+      (setq ispell-program-name "hunspell"
+            ispell-extra-args '("-D en_US")
+            )
+      )))
+
+
+;;----------------------------------------------------------------------------
+;; Add spell-checking in comments for all programming language modes
+;;----------------------------------------------------------------------------
+(dolist (hook '(lisp-mode-hook
+                emacs-lisp-mode-hook
+                scheme-mode-hook
+                clojure-mode-hook
+                ruby-mode-hook
+                yaml-mode
+                python-mode-hook
+                shell-mode-hook
+                php-mode-hook
+                css-mode-hook
+                haskell-mode-hook
+                caml-mode-hook
+                c++-mode-hook
+                c-mode-hook
+                lua-mode-hook
+                crontab-mode-hook
+                perl-mode-hook
+                tcl-mode-hook
+                js2-mode-hook))
+  (add-hook hook 'flyspell-prog-mode))
+
+;; you can also use "M-x ispell-word" or hotkey "M-$". It pop up a multiple choice
+;; @see http://frequal.com/Perspectives/EmacsTip03-FlyspellAutoCorrectWord.html
+(global-set-key (kbd "C-c s") 'flyspell-auto-correct-word)
+
+(provide 'init-spelling)
